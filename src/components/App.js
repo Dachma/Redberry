917aae4871d7c16afef146192f498c7f91a1a6a5
Dachi Kvirikadze <dachikvirikadze01@gmail.com>
2023-02-03 19:32:04 +0400

    added BrowserRouter and created landingPage.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LandingPage from './LandingPage';
 
 class App extends React.Component {
     render() { 
         return (
             <div>
-                s
+                <BrowserRouter>
+                    <Routes>
+                        <Route path='/' exact element={<LandingPage/>}/>
+                    </Routes>
+                </BrowserRouter>
             </div>
         );
     }
