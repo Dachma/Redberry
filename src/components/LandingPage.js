d8a0c2e7f494f4f840bfeaf263d31c6027b65148
Dachi Kvirikadze <dachikvirikadze01@gmail.com>
2023-02-03 20:19:13 +0400

    finished landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 class LandingPage extends React.Component {
     render() { 
         return (
-            <div>
-                hi
+            <div className="landing-page">
+                <div className='redberry-logo'/>
+                <div className='line'/>
+                <div className='logo'/>
+                <Link to='/info'>
+                    <button className='add-button'>
+                        <div className='add-resume-txt'>
+                            რეზიუმეს დამატება
+                        </div>
+                    </button>
+                </Link>
             </div>
         );
     }
